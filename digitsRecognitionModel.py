b97d1b76b186cf99edffdbac763f0254e9b400ed
Kavindu Rathnayaka <KavinduReaper@users.noreply.github.com>
2020-12-29 16:51:59 +0530

    Initial commit

---
+++ digitsRecognitionModel.py
@@ -0,0 +1,67 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+if __name__ == '__main__':
+    # the MNIST data is split between train and test sets
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    # Reshape to be samples*pixels*width*height
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
+
+    # One hot Cpde
+    y_train = np_utils.to_categorical(y_train)
+    y_test = np_utils.to_categorical(y_test)
+    num_classes = y_test.shape[1]
+
+    # convert from integers to floats
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    # normalize to range [0, 1]
+    X_train = (X_train / 255.0)
+    X_test = (X_test / 255.0)
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+    # model.summary()
+
+    # compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("Large CNN Error: %.2f%%" % (100 - scores[1] * 100))
+
+    test_images = X_test[1:5]
+    test_images = test_images.reshape(test_images.shape[0], 28, 28)
+    print("Test images shape: {}".format(test_images.shape))
+    for i, test_image in enumerate(test_images, start=1):
+        org_image = test_image
+        test_image = test_image.reshape(1, 28, 28, 1)
+        prediction = model.predict_classes(test_image, verbose=0)
+        print("Predicted digit: {}".format(prediction[0]))
+        plt.subplot(220 + i)
+        plt.axis('off')
+        plt.title("Predicted digit: {}".format(prediction[0]))
+        plt.imshow(org_image, cmap=plt.get_cmap('gray'))
+    plt.show()
+
+    # save model and architecture to single file
+    model.save("model.h5")
+    print("Saved model to disk")
+
+    accuracy = model.evaluate(x=X_test, y=y_test, verbose=0)
+    print("Accuracy: ", accuracy)
